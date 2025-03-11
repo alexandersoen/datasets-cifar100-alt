a87a2beea8d15a544e1f0670fd8e951fea5cea9d
Alexander Soen <alexandersoen@gmail.com>
2025-03-11 17:38:22 +0900

    Fix check script for data source

--- check.py
+++ check.py
@@ -3,15 +3,19 @@ Script to check the statistics of the generated datasets.
 """
 
 import tensorflow_datasets as tfds
+from tensorflow_datasets.core import FileFormat
 
 
 def generate_hist(builder_str: str, split_str: str = "train") -> list[int]:
     builder = tfds.builder(builder_str)
-    dataset = builder.as_dataset(split=split_str)
+    if builder.info.file_format == FileFormat.ARRAY_RECORD:
+        data = builder.as_data_source(split=split_str)
+    else:
+        data = builder.as_dataset(split=split_str).as_numpy_iterator()  # pyright: ignore
 
     hist = [0] * 100
-    for example in dataset.as_numpy_iterator():  # pyright: ignore
-        hist[int(example["label"])] += 1
+    for example in data:
+        hist[int(example["label"])] += 1  # pyright: ignore
 
     return hist
 
@@ -26,3 +30,4 @@ if __name__ == "__main__":
     print(generate_hist("cifar100_longtail/head_100"))
     print(generate_hist("cifar100_longtail/head_50"))
     print(generate_hist("cifar100_longtail/head_25"))
+
